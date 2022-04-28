ebf9956eae4356b4748e856db6a66a59a293878d
CrioUser <crio-user@criodo.com>
2022-04-28 19:24:23 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://localhost:8082" };
+const config = { backendEndpoint: "http://43.204.66.92:8084" };
 
 export default config;
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -3,10 +3,10 @@ import config from "../conf/index.js";
 async function init() {
   //Fetches list of all cities along with their images and description
   let cities = await fetchCities();
-  debugger;
+  // debugger;
   //Updates the DOM with the cities
   cities.forEach((key) => { 
-    debugger;
+    // debugger;
     addCityToDOM(key.id, key.city, key.description, key.image);
   });
  
@@ -14,15 +14,25 @@ async function init() {
 // console.log("From init()", init());
 
 //Implementation of fetch call
+
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-  const res = await fetch('http://3.110.34.3:8082/cities').catch(error => {
-    console.error('Error:', error);
-  });
-  let items = await res.json();
-  return items;
+  try{
+    const res = await fetch('http://43.204.66.92:8084');
+    let items = await res.json();
+    if(!res.ok){
+      throw new Error(`HTTP error: ${res.status}`);
+    }
+    return items;
+  }
+  catch(err){
+    console.log('Error:', err);
+    return null;
+  }
+ 
 }
+
 
 
 //Implementation of DOM manipulation to add cities
@@ -31,7 +41,7 @@ function addCityToDOM(id, city, description, image) {
   // 1. Populate the City details and insert those details into the DOM
   let container = document.createElement('div');
   container.className = "col-12 col-sm-6 col-lg-3 mb4";
-  let innerHTML =`<a href="#">
+  let innerHTML =`<a href="adventures/?city=london" id='london'>
               <div class="text-center tile-text">
                 <h5>${city}</h5> 
                 <p>${description}</p>
